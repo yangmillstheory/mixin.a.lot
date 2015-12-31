e18981be9e1d8c0922aa6c7cdb5a15b3015470a5
Victor Alvarez <v.alvarez312@gmail.com>
2015-12-30 17:30:33 -0800

    Cleaning Mixin class

--- src/mixin/index.ts
+++ src/mixin/index.ts
@@ -12,19 +12,20 @@ export class Mixin {
         Has no special behavior or data other than immutability and
 
         - name
-        - toString()
         - mixin_keys
             (Array of property names that to mix in)
-        - pre/post mixinghooks (optional)
-            (functions invoked with the mixtarget context before/after mixing)
+        - toString()
 
         The only way to create instances is through the factory method .from_obj.
-     */
+    */
     
     public name: string;
     public mixin_keys: string[];
     
-    private constructor(name: string) {
+    public pre_mixing_hook:  (thing: Mixin | typeof Mixin) => void;
+    public post_mixing_hook: (thing: Mixin | typeof Mixin) => void;
+    
+    public constructor(name: string) {
         this.name = name;
     }
     
@@ -33,22 +34,21 @@ export class Mixin {
             .join(', ')})`;
     }
     
-    public from_obj(spec: MixinSpec, freeze: boolean = true): Mixin {
+    public static from_obj(spec: MixinSpec, freeze: boolean = true): Mixin {
         if (!_lang.isObject(spec) || Array.isArray(spec)) {
             throw new TypeError("Expected non-empty object literal");
         }
-        let name = spec.name;
-        if (typeof name !== 'string') {
-            throw new ValueError("Expected String name in mixin object")
-        }
-        delete spec.name;
-        let mixin = new Mixin(name);
-        let mkeys = Object.keys(spec).sort()
-        if (!mkeys.length) {
+        let keys = Object.keys(spec).sort()
+        if (!keys.length) {
             throw new ValueError('Found nothing to mix in!')
         }
-        for (let key in mkeys) {
-            if (!mkeys.hasOwnProperty(key)) {
+        let name = spec.name;
+        if (typeof name !== 'string') {
+            throw new ValueError("Expected String name in mixin spec")
+        }
+        let mixin = new Mixin(name);
+        for (var key in keys) {
+            if (key === 'name' || !keys.hasOwnProperty(key)) {
                 continue;
             }
             Object.defineProperty(mixin, key, {
@@ -66,47 +66,11 @@ export class Mixin {
         }
         return mixin;
     }
-
-
-//     for own hook_name, hook of @_parse_mixinghooks(mixin)
-//       if hook? && !_.isFunction hook
-//         throw new TypeError "Expected a function for #{hook_name}"
-
-//   @premixing_hook_keys = [
-//     'premixing_hook'
-//     'premixing'
-//     'premix'
-//   ]
-
-//   @postmixing_hook_keys = [
-//     'postmixing_hook'
-//     'postmixing'
-//     'postmix'
-//   ]
-
-//   @_parse_mixinghooks: (mixin) ->
-//     mixinghooks = {}
-//     before = _.find @premixing_hook_keys, (alias) -> _.has(mixin, alias)
-//     after = _.find @postmixing_hook_keys, (alias) -> _.has(mixin, alias)
-//     if before?
-//       mixinghooks[before] = mixin[before]
-//     if after?
-//       mixinghooks[after] = mixin[after]
-//     mixinghooks
-
-
-
-//   get_postmixing_hook: ->
-//     hook_key = _.find @constructor.postmixing_hook_keys, (key) => @[key]?
-//     @[hook_key]
-
-//   get_premixing_hook: ->
-//     hook_key = _.find @constructor.premixing_hook_keys, (key) => @[key]?
-//     @[hook_key]
-
+}
 
 Object.freeze(Mixin);
 Object.freeze(Mixin.prototype);
 
-
-export {make: Mixin.from_obj.bind(Mixin)};
+export var make = (spec: MixinSpec) => {
+    return  Mixin.from_obj(spec);
+}
