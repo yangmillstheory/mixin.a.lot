221f0cc093ebceba88bf07f52c1ed7ade49cf77e
Victor Alvarez <v.alvarez312@gmail.com>
2016-01-01 22:48:30 -0800

    cleaning

--- src/spec-utils/index.ts
+++ src/spec-utils/index.ts
@@ -1,22 +1,22 @@
 import * as _ from 'lodash';
 import {make_mixin} from '../mixin';
 
-export var beforeOnce = fn => {
-    return beforeEach(_.once(fn));
+
+let make_spyable_method = (mixin: Mixin, method_name: string): void => {
+    mixin.mixin_keys.push(method_name);
+    mixin[method_name] = _.noop;
 };
 
-export const MIXINS = {
-    _make_spyable_method: (mixin: Mixin, method_name: string): void => {
-        mixin.mixin_keys.push(method_name);
-        mixin[method_name] = _.noop;
-    },
-    default_mixin: function(): Mixin {
-        let mixin: Mixin = make_mixin({
-            name: 'Default Example Mixin',
-            foo: 'foo',
-            bar: 1,
-        });
-        this._make_spyable_method(mixin, 'baz');
-        return mixin;
-    },
+export var default_mixin = (): Mixin => {
+    let mixin: Mixin = make_mixin({
+        name: 'Default Example Mixin',
+        foo: 'foo',
+        bar: 1,
+    });
+    make_spyable_method(mixin, 'baz');
+    return mixin;
+};
+
+export var beforeOnce = fn => {
+    return beforeEach(_.once(fn));
 };
