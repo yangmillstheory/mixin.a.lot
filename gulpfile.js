3bdbaa9ae93ee337ed3e9a735877066c804bac0f
Victor Alvarez <v.alvarez312@gmail.com>
2016-01-02 19:21:35 -0800

    linting tests with alternate spec

--- gulpfile.js
+++ gulpfile.js
@@ -4,65 +4,46 @@ let tslint = require('gulp-tslint');
 let del = require('del');
 let child_process = require('child_process');
 let karma = require('karma');
-let _ = require('lodash');
 
-
-let series = gulp.series; 
-let parallel = gulp.parallel; 
+let series = gulp.series;
+let parallel = gulp.parallel;
 let task = gulp.task;
-    
-const SRC = {
-    base: 'src',
-    ts() {
-        return [
-            `${this.base}/**/*.ts`,
-            `!${this.base}/*.spec.ts`,
-            `!${this.base}/spec-utils.ts`
-        ];
-    },
-    spec() {
-        return [
-            `${this.base}/*.spec.ts`,
-            `${this.base}/spec-utils.ts`
-        ];
-    }
-};
 
+const SRC = {base: 'src'};
 const DIST = {base: 'dist'};
-    
-let do_tslint = (globs, options) => {
-    if (!options) {
-        options = {};
-    }
-    _.defaults(options, {
-        emitError: false,
-        summarizeFailureOutput: true
-    })
-    return gulp.src(globs)
-        .pipe(tslint())
-        .pipe(tslint.report("verbose", options));
-};
+
 
 task('compile', (done) => {
-    child_process.exec('node_modules/.bin/tsc', done);
+  child_process.exec('node_modules/.bin/tsc', done);
 });
 
 task('clean', (done) => {
-    del([`${DIST.base}/**/*`], {force: true}, done);
+  del([`${DIST.base}/**/*`], {force: true}, done);
 });
 
 task('test', (done) => {
-    new karma.Server({
-        configFile: __dirname + '/karma.conf.js'
-    }, done).start();
+  new karma.Server({
+    configFile: __dirname + '/karma.conf.js'
+  }, done).start();
 });
 
-task('lint:ts', () => {
-    return do_tslint(SRC.ts());
+let tslint_stream = (glob, config_file) => {
+  return gulp.src(glob)
+    .pipe(tslint({
+        configuration: config_file
+    }))
+    .pipe(tslint.report('verbose', {
+        summarizeFailureOutput: true,
+        emitError: false
+    }));
+}
+
+task('lint:ts', (done) => {
+  return tslint_stream(`${SRC.base}/**/!(*spec).ts`, './tslint.json');
 });
 
-task('lint:spec', () => {
-    return do_tslint(SRC.spec());
+task('lint:spec', (done) => {
+  return tslint_stream(`${SRC.base}/**/*.spec.ts`, './tslint.spec.json');
 });
 
 task('lint', parallel('lint:ts', 'lint:spec'));
