915e3aa92cc0ad7940aee3a353c4b30a2dd6cf3d
Victor Alvarez <v.alvarez312@gmail.com>
2016-01-10 20:50:49 -0800

    write and pass some key tests
    - method advice is always invoked on the target
    - method advice is called in the right order
    - method advice are adapters to and from mixin method

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,7 @@
 import {expect} from 'chai';
 import {spy, stub} from 'sinon';
 import {mix} from './index';
-import {default_mixin} from './spec-utils';
+import {logger_mixin} from './spec-utils';
 
 describe('mixing', () => {
 
@@ -13,23 +13,22 @@ describe('mixing', () => {
     });
   });
 
-  it('should objects or function targets', () => {
+  it('should expect objects or function targets', () => {
     [1, 'string', true, null, undefined].forEach(target => {
       expect(() => {
-        mix(target, default_mixin());
+        mix(target, logger_mixin());
       }).to.throw(TypeError, `Expected non-null object or function, got ${target}`);
     });
   });
 
   it('should mix into the target', () => {
     let target = {};
-    let mixin = default_mixin();
-    stub(mixin, 'baz').returns(['baz']);
+    let logger = logger_mixin();
 
-    mix(target, mixin);
-
-    expect(target.bar).to.equal(1);
-    expect(target.baz()).to.deep.equal(['baz']);
+    mix(target, logger);
+    Object.getOwnPropertyNames(logger).forEach((key: string) => {
+      expect(target[key]).to.equal(logger[key]);
+    });
   });
 
   it('should mix in order', () => {
@@ -45,17 +44,27 @@ describe('mixing', () => {
     expect(target.qux).to.equal('baz');
   });
 
+  it('should call mixin methods on the target context', () => {
+    let mixin = {method: spy()};
+    let target = {};
+
+    mix(target, mixin);
+
+    target.method();
+    expect(target.method.calledOn(target)).to.be.true;
+  });
+
   describe('mixing advice', () => {
 
     it('should expect functions as mixing advice', () => {
-      let mixin = default_mixin();
+      let mixin = logger_mixin();
       mixin.pre_mixing_hook = 1;
 
       expect(() => {
         mix({}, mixin);
       }).to.throw(TypeError, 'Expected a function for pre_mixing_hook');
 
-      mixin = default_mixin({
+      mixin = logger_mixin({
         post_mixing_hook: []
       });
 
@@ -68,7 +77,7 @@ describe('mixing', () => {
 
       it('should call it on the mixin with the target as a parameter', () => {
         let target = {};
-        let mixin = default_mixin({
+        let mixin = logger_mixin({
           pre_mixing_hook: spy()
         });
         mix(target, mixin);
@@ -84,8 +93,8 @@ describe('mixing', () => {
         let pre_mixing_hook = stub();
         pre_mixing_hook.throws(error);
 
-        let mixin = default_mixin({pre_mixing_hook});
-        expect(mixin.foo).to.exist;
+        let mixin = logger_mixin({pre_mixing_hook});
+        expect(mixin.log).to.exist;
 
         try {
           mix(target, mixin);
@@ -93,14 +102,14 @@ describe('mixing', () => {
           threw = true;
         } finally {
           expect(threw).to.be.true;
-          expect(target.foo).not.to.exist; // wasn't mixed in!
+          expect(target.log).not.to.exist; // wasn't mixed in!
         }
       });
 
       it('should not mix it into the target', () => {
         let target = {};
-        let mixin = default_mixin({
-          pre_mixing_hook: new Function()
+        let mixin = logger_mixin({
+          pre_mixing_hook: spy()
         });
 
         mix(target, mixin);
@@ -114,7 +123,7 @@ describe('mixing', () => {
 
       it('should call it on the mixin with the target as a parameter', () => {
         let target = {};
-        let mixin = default_mixin({
+        let mixin = logger_mixin({
           post_mixing_hook: spy()
         });
         mix(target, mixin);
@@ -130,8 +139,8 @@ describe('mixing', () => {
         let post_mixing_hook = stub();
         post_mixing_hook.throws(error);
 
-        let mixin = default_mixin({post_mixing_hook});
-        expect(mixin.bar).to.equal(1);
+        let mixin = logger_mixin({post_mixing_hook});
+        expect(mixin.log).to.exist;
 
         try {
           mix(target, mixin);
@@ -139,13 +148,13 @@ describe('mixing', () => {
           threw = true;
         } finally {
           expect(threw).to.be.true;
-          expect(target.bar).to.equal(1); // was mixed in before throwing
+          expect(target.log).to.equal(mixin.log); // was mixed in before throwing
         }
       });
 
       it('should not mix it into the target', () => {
         let target = {};
-        let mixin = default_mixin({
+        let mixin = logger_mixin({
           post_mixing_hook: new Function()
         });
 
@@ -161,17 +170,21 @@ describe('mixing', () => {
   describe('mix options', () => {
 
     beforeEach(() => {
-      this.mixin = default_mixin();
+      this.mixin = logger_mixin({
+        err_log: '/logs/app.err',
+        inf_log: '/logs/app.log',
+      });
     });
 
     describe('omitting mixin methods', () => {
 
       it('should omit some mixin keys', () => {
         let target = {};
-        mix(target, this.mixin, {omit: ['bar']});
+        mix(target, this.mixin, {omit: ['err_log', 'inf_log']});
 
-        expect(target.bar).to.not.exist;
-        expect(target.baz).to.exist;
+        expect(target.err_log).to.not.exist;
+        expect(target.inf_log).to.not.exist;
+        expect(target.log).to.exist;
       });
 
       it('should expect a non-empty Array for omissions', () => {
@@ -188,23 +201,23 @@ describe('mixing', () => {
         }).to.throw("Some omit keys aren't in mixin: non_mixin_key");
       });
 
-      it('should not mangle the prototype hierarchy when omitting keys', () => {
+      it('should not change the prototype chain when omitting keys', () => {
         let target = {};
-        target.__proto__ = Object.create({
-          bar() {
-            return 'protobar';
+        Object.setPrototypeOf(target, {
+          log() {
+            return 'protolog';
           },
         });
 
-        mix(target, this.mixin, {omit: ['bar']});
+        mix(target, this.mixin, {omit: ['log']});
 
-        expect(target.bar).to.exist;
-        expect(target.bar()).to.equal('protobar');
+        expect(target.log).to.exist;
+        expect(target.log()).to.equal('protolog');
       });
 
       it('should not omit all mixin keys', () => {
         expect(() => {
-          mix({}, this.mixin, {omit: ['bar', 'baz', 'foo']});
+          mix({}, this.mixin, {omit: Object.getOwnPropertyNames(this.mixin)});
         }).to.throw('All mixin keys have been omitted!');
       });
 
@@ -224,86 +237,174 @@ describe('mixing', () => {
 
       it('should expect functions as values in method advice', () => {
         [
-          {method_1: 1},
-          {
-            method_1: true,
-            method_2: new Function(),
-          },
-          {method_1: null},
-          {method_1: 'string'},
+          {log: 1},
+          {log: true},
+          {log: null},
+          {log: 'string'},
         ].forEach(pre_method_advice => {
           expect(() => {
             mix({}, this.mixin, {pre_method_advice});
-          }).to.throw(TypeError, "pre_method_advice for method_1 isn't a function");
+          }).to.throw(TypeError, "pre_method_advice for log isn't a function");
         });
       });
 
       it('should expect methods that are in the mixin', () => {
-        [
+        interface IErrorFixture {
+          pre_method_advice: Object;
+          expected_error: string;
+        }
+
+        let fixtures: IErrorFixture[] = [
           {
             pre_method_advice: {
-              baz: new Function(),    // valid method
-              non_existent_method_1: new Function(),  // invalid
-              non_existent_method_2: new Function(),  // invalid
+              log: new Function(),                  // valid method
+              non_existent_method: new Function(),  // invalid - not in mixin
             },
-            not_in_mixin: 'non_existent_method_1',
+            expected_error: `non_existent_method isn't a method on ${this.mixin}`,
           },
           {
             pre_method_advice: {
-              baz: new Function(), // valid method
-              foo: new Function(), // non-method property
+              log: new Function(),     // valid method
+              err_log: new Function(), // non-function property
             },
-            not_in_mixin: 'foo',
+            expected_error: `err_log isn't a method on ${this.mixin}`,
           },
-        ].forEach(fixture => {
-          let {pre_method_advice, not_in_mixin} = fixture;
+        ];
+
+        fixtures.forEach((fixture: IErrorFixture) => {
           expect(() => {
-            mix({}, this.mixin, {pre_method_advice});
-          }).to.throw(`${not_in_mixin} isn't a method on ${this.mixin}`);
+            mix({}, this.mixin, {
+              pre_method_advice: fixture.pre_method_advice
+            });
+          }).to.throw(fixture.expected_error);
         });
       });
 
-      it('should call pre_method_advice before the method on the target', () => {
-        stub(this.mixin, 'baz', value => {
-          return [value];
-        });
+      describe('pre_method_advice', () => {
 
-        let options = {
-          pre_method_advice: {
-            baz() {
-              return 'before_baz';
+        it('should be called on the target before the mixin method', () => {
+          let target = {};
+          let pre_advice_spy = spy();
+
+          spy(this.mixin, 'log');
+
+          mix(target, this.mixin, {
+            pre_method_advice: {
+              log: pre_advice_spy
             },
-          },
-        };
-        spy(options.pre_method_advice, 'baz');
+          });
 
-        let target = {};
-        mix(target, this.mixin, options);
+          target.log();
 
-        expect(target.baz()).to.deep.equal(['before_baz']);
-        expect(options.pre_method_advice.baz.calledOn(target)).to.be.true;
-      });
-
-      it('should call post_method_advice after the method on the target', () => {
-        stub(this.mixin, 'baz', baz => {
-          return ['baz'];
+          expect(this.mixin.log.calledOnce).to.be.true;
+          expect(pre_advice_spy.calledOnce).to.be.true;
+          expect(pre_advice_spy.calledBefore(this.mixin.log)).to.be.true;
         });
 
-        let options = {
-          post_method_advice: {
-            baz(baz) {
-              return baz.concat(['after_baz']);
+        it('should be an adapter to the mixin method', () => {
+          let mock_console = {
+            log: spy()
+          };
+
+          let options: IMixOptions = {
+            pre_method_advice: {
+              // prefix every message with ERROR or INFO
+              log(error: Error, message: string): string {
+                if (error) {
+                  return `ERROR:${message}`;
+                }
+                return `INFO:${message}`;
+              },
             },
-          },
-        };
-        spy(options.post_method_advice, 'baz');
+          };
 
-        let target = {};
-        mix(target, this.mixin, options);
+          stub(this.mixin, 'log', function(message: string): void {
+            mock_console.log(`${this.logname}:${message}`);
+          });
 
-        expect(target.baz()).to.deep.equal(['baz', 'after_baz']);
-        expect(options.post_method_advice.baz.calledOn(target)).to.be.true;
+          let target = {
+            logname: 'prefixing_logger'
+          };
+          mix(target, this.mixin, options);
+
+          target.log(null, 'A-OK.');
+          expect(mock_console.log.calledWithExactly('prefixing_logger:INFO:A-OK.'))
+            .to.be.true;
+
+          target.log(new Error, 'Uh oh.');
+          expect(mock_console.log.calledWithExactly('prefixing_logger:ERROR:Uh oh.'))
+            .to.be.true;
+        });
+
       });
+
+      describe('post_method_advice', () => {
+
+        it('should be called on the target after the mixin method', () => {
+          let target = {};
+          let post_advice_spy = spy();
+
+          spy(this.mixin, 'log');
+
+          mix(target, this.mixin, {
+            post_method_advice: {
+              log: post_advice_spy
+            },
+          });
+
+          target.log();
+
+          expect(this.mixin.log.calledOnce).to.be.true;
+          expect(post_advice_spy.calledOnce).to.be.true;
+          expect(post_advice_spy.calledAfter(this.mixin.log)).to.be.true;
+        });
+
+        it('should adapt to the mixin method', () => {
+          let mock_console = {
+            log: spy(),
+            info: spy(),
+            error: spy(),
+          };
+
+          let options: IMixOptions = {
+            post_method_advice: {
+              // log to console too
+              log(log_result: {error: boolean, message: string}): void {
+                if (log_result.error) {
+                  mock_console.error(log_result.message);
+                }
+                mock_console.info(log_result.message);
+              },
+            },
+          };
+
+          stub(this.mixin, 'log', function(error: Error, message: string): {
+            error: boolean,
+            message: string,
+          } {
+            mock_console.log(`${this.logname}:${message}`);
+            return {
+              error: !!error,
+              message,
+            };
+          });
+
+          let target = {
+            logname: 'console_logger'
+          };
+          mix(target, this.mixin, options);
+
+          target.log(null, 'Hello, World!');
+          expect(mock_console.info.calledWithExactly('Hello, World!'))
+            .to.be.true;
+
+          target.log(new Error, 'Something bad happened.');
+          expect(mock_console.error.calledWithExactly('Something bad happened.'))
+            .to.be.true;
+        });
+
+      });
+
 
     });
 
--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,13 @@ const USAGE = (target?): string => {
   return `Expected non-null object or function, got ${target}`;
 };
 
-export var mix = function(target, mixin: Mixin, options: MixOptions = {}) {
+interface IMixJoint {
+  method: Function;
+  advice: Function;
+  key: string;
+}
+
+export var mix = function(target, mixin: IMixin, options: IMixOptions = {}) {
   if (target === undefined) {
     throw new TypeError(USAGE());
   } else if (!(is_function(target) || is_object(target))) {
@@ -27,25 +33,36 @@ export var mix = function(target, mixin: Mixin, options: MixOptions = {}) {
     pre_mixing_hook, post_mixing_hook,
     omit,
   } = parse_ioptions(options, mixin);
+  let keys_to_mix_in = diff_arrays(
     Object.getOwnPropertyNames(mixin).filter(key => {
       return (mixin[key] !== pre_mixing_hook) && (mixin[key] !== post_mixing_hook);
     }),
     omit);
-  if (is_empty(mixing_keys)) {
+  if (is_empty(keys_to_mix_in)) {
     throw new Error('All mixin keys have been omitted!');
   }
   pre_mixing_hook.call(mixin, target);
-  mixing_keys.forEach((key: string) => {
-    if (pre_method_advice[key]) {
-      let bound_advice = pre_method_advice[key].bind(target);
-      let mixin_method = mixin[key];
-      target[key] = compose(mixin_method, bound_advice);
-    } else if (post_method_advice[key]) {
-      let bound_advice = post_method_advice[key].bind(target);
-      let mixin_method = mixin[key];
-      target[key] = compose(bound_advice, mixin_method);
-    } else {
+  let mix_joints: IMixJoint[] = [];
+  keys_to_mix_in.forEach((key: string) => {
+    let advice = pre_method_advice[key] || post_method_advice[key];
+    if (advice) {
+      // defer binding the composite methods until
+      // after all other data/behavior have been mixed in
+      mix_joints.push({
+        advice,
+        method: mixin[key],
+        key: key,
+      });
+    } else  {
       target[key] = mixin[key];
+    }
+  });
+  mix_joints.forEach(composite => {
+    let {advice, method, key} = composite;
+    if (pre_method_advice[key]) {
+      target[key] = compose(method, advice, target);
+    } else if (post_method_advice[key]) {
+      target[key] = compose(advice, method, target);
     }
   });
   post_mixing_hook.call(mixin, target);
